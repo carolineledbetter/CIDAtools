33b2f3a7b9f06b4f66d70feda66b7b75e91edb87
Caroline Ledbetter <caroline.ledbetter@ucdenver.edu>
2018-06-07 11:19:43 -0600

    removed redundant code

--- CIDAtools/R/MiscFxns.R
+++ CIDAtools/R/MiscFxns.R
@@ -18,7 +18,6 @@ setAnalyst <- function(AnalystName){
     AnalystName <- AnalystName[1]
   }
   setPermanentAnalyst(AnalystName)
-  options(CIDAtools.analyst = AnalystName)
   site_path = R.home(component = "home")
   Project_setup <- paste0(site_path,
                           '/library/CIDAtools/rstudio/',
