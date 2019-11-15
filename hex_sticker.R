0871ed97a1a9d5893369650f6f679c9c2d9ba755
Caroline Ledbetter <caroline.ledbetter@ucdenver.edu>
2019-11-15 11:44:42 -0700

    add url for tools svg

--- hex_sticker.R
+++ hex_sticker.R
@@ -1,5 +1,7 @@
 library(hexSticker)
-imgurl <- "~/Downloads/2000px-Tools-spanner-hammer.svg.png"
+imgurl <- paste0("https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/",
+                 "Tools-spanner-hammer.svg/",
+                 "2000px-Tools-spanner-hammer.svg.png")
 sticker(imgurl, package="CIDAtools",
         s_x = 1,
         s_y = 1,
